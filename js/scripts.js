a9e20ec88fd3ce941bec52c3d850d32590a9def3
Alesandria Wild <alesandriawild@yahoo.com>
2022-08-13 20:15:56 -0700

    WIP getting quiz to run multiple branches.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,31 @@
 function hideResults(){
+  document.getElementById("output").setAttribute("class", "hidden");
   document.getElementById("ruby").setAttribute("class", "hidden");
-  document.getElementById("see-sharp").setAttribute("class", "hidden");
   document.getElementById("python").setAttribute("class", "hidden");
+  document.getElementById("csharp").setAttribute("class", "hidden");
 }
 
-function submitQuestions(e) {
-  e.preventDefault();
-  hideResults();
-  const input1 = document.querySelector("input[name='difficulty']:checked").value;
-  const input2 = document.querySelector("input[name='creativity-level']:checked").value;
-  const input3 = document.querySelector("input[name='company-type']:checked").value;
-  const input4 = document.querySelector("input[name='project-size']:checked").value;
-  const input5 = document.querySelector("input[name='']:checked").value;
+function checkLanguage(a, b, c, d, e){
+  const newList = [];
+  
 
-  if (input1 === '1' && (input2 === '' || input3 === '' || input4 === '' || input5 === '')){
-    document.getElementById("ruby").removeAttribute("class");
-  } else if (input1 === '' && input2 === ''){
-    document.getElementById("see-sharp").removeAttribute("class");
-  } else if ((input1 ==='' && (input2 === '' || input2 === '')) || (input1 === '' && (input2 === '' || input2 ==='' || input2 === ''))){
-    document.getElementById("python").removeAttribute("class");
+  if (input5 === '1') {
+    return "Ruby on Rails!";
+  } else if (input5 === '2') {
+    return "C#!";
+  } else if (input5 ==='3') {
+    return "Python!";
   }
 }
 
-window.addEventListener("load",function( {
-  const form = this.document.getElementById("quiz-input")
-  form.addEventListener("submit", submitQuestions);
-}));
+window.addEventListener("load", function() {
+  const form = document.getElementById("quiz-input");
+  document.querySelector("form").onsubmit = function(e){
+    e.preventDefault();
+  let a = document.querySelector("difficulty#difficulty-").value;
+  let b = document.querySelector().value;
+  let c = document.querySelector().value;
+  const result = checkLanguage(a, b, c, d, e);
+  document.getElementById("output").innerText = result;
+  }
+});
